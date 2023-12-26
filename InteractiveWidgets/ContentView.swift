5d614319f20e0a176d17c3725dfb15dd30dc6dae
IvanL <antila3567@gmail.com>
2023-12-26 18:56:27 +0200

    Initial Commit

---
+++ InteractiveWidgets/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InteractiveWidgets
+//
+//  Created by Иван Легенький on 26.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InteractiveWidgets/InteractiveWidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveWidgetsApp.swift
+//  InteractiveWidgets
+//
+//  Created by Иван Легенький on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveWidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
